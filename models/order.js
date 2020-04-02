347e18f680306d4c793b11fef065339829e101e3
dazuo <863329112@qq.com>
2020-04-02 20:11:10 +0800

    新增-订单列表

--- models/order.js
+++ models/order.js
@@ -1,4 +1,4 @@
-import { get, post } from '../utils/request'
+import { get, post, put, _delete } from '../utils/request'
 
 class Order {
 
@@ -6,6 +6,23 @@ class Order {
     return post('/api/placeorder', {
       addressId, couponLogId, dispatchAmount, expectAmount, goodsList
     })
+  }
+
+  // 状态: -1 已取消 0-待付款 1-待发货 2-待收货 3-已完成 4-（待发货）退款申请 5-已退款
+  async getOrderList(status, page, size) {
+    return get(`/api/order/list?status=${status}&page=${page}&size=${size}`)
+  }
+
+  async deleteOrder(id) {
+    return _delete(`/api/order?id=${id}`)
+  }
+
+  async cancelOrder(id) {
+    return put(`/api/order/cancel?id=${id}`)
+  }
+
+  async confirmTakeGoods(id) {
+    return put(`/api/order/confirm_goods?id=${id}`)
   }
 }
 
--- pages/order/index.js
+++ pages/order/index.js
@@ -1,64 +1,112 @@
+import order from '../../models/order'
+
 Page({
   data: {
-    orderList: [
-      {
-        id: 1,
-        orderNo: '234238090984',
-        goodsNum: 4,
-        payAmount: '139.80',
-        goodsList: [
-          {
-            goodsId: 1,
-            picture: 'https://dcdn.it120.cc/2019/12/29/2e79921a-92b3-4d1d-8182-cb3d524be5fb.png',
-            title: '北欧简约立式台灯北欧简约立式',
-            specs: '3m;黑色;可调节',
-            price: '289',
-            num: 1
-          },
-          {
-            goodsId: 1,
-            picture: 'https://dcdn.it120.cc/2019/12/29/2e79921a-92b3-4d1d-8182-cb3d524be5fb.png',
-            title: '北欧简约立式台灯北欧简约立式',
-            specs: '3m;黑色;可调节',
-            price: '289',
-            num: 1
-          }
-        ]
-      },
-      {
-        id: 1,
-        orderNo: '234238090984',
-        goodsNum: 4,
-        payAmount: '139.80',
-        goodsList: [
-          {
-            goodsId: 1,
-            picture: 'https://dcdn.it120.cc/2019/12/29/2e79921a-92b3-4d1d-8182-cb3d524be5fb.png',
-            title: '北欧简约立式台灯北欧简约立式',
-            specs: '3m;黑色;可调节',
-            price: '289',
-            num: 1
-          },
-          {
-            goodsId: 1,
-            picture: 'https://dcdn.it120.cc/2019/12/29/2e79921a-92b3-4d1d-8182-cb3d524be5fb.png',
-            title: '北欧简约立式台灯北欧简约立式',
-            specs: '3m;黑色;可调节',
-            price: '289',
-            num: 1
-          }
-        ]
-      }
-    ]
+    status: 0,
+    curPage: 0,
+    pageSize: 10,
+    orderList: []
   },
   onLoad(res) {
-    // 查询全部：{status: "undefined"}
-    console.log(res)
+    this.setData({
+      status: parseInt(res.status, 10) || 0
+    })
   },
-  orderPay(e) {
-    console.log(e.currentTarget.dataset.orderno)
+  onShow() {
+    this.loadOrderList()
   },
-  deleteOrder(e) {
-    console.log(e.currentTarget.dataset.orderno)
+  // 加载订单
+  async loadOrderList() {
+    const curPage = this.data.curPage + 1
+    const res = await order.getOrderList(this.data.status, curPage, this.data.pageSize)
+    const { error_code, msg } = res
+    if (error_code !== undefined) {
+      console.log(msg)
+      return      
+    }
+    let orderList = this.data.orderList
+    if (curPage === 1) {
+      orderList = res.list
+    } else {
+      orderList = orderList.concat(res.list)
+    }
+    this.setData({
+      orderList: orderList
+    })
+  },
+  // 拉起支付窗口
+  async orderPay(e) {
+    console.log(e.currentTarget.dataset.id)
+  },
+  async cancelOrder(e) {
+    const that = this
+    wx.showModal({
+      title: '取消订单',
+      content: '确认取消订单？',
+      success(res) {
+        if (res.confirm) {
+          const orderId = e.currentTarget.dataset.id
+          order.cancelOrder(orderId).then(res => {
+            const { error_code, msg } = res
+            if (error_code !== undefined) {
+              console.log(msg)
+              return
+            }
+            that.loadOrderList()
+          })
+        }
+      }
+    })
+  },
+  async deleteOrderRecord(e) {
+    const that = this
+    wx.showModal({
+      title: '删除订单',
+      content: '确认删除订单？',
+      success(res) {
+        if (res.confirm) {
+          const orderId = e.currentTarget.dataset.id
+          order.deleteOrder(orderId).then(res => {
+            const { error_code, msg } = res
+            if (error_code !== undefined) {
+              console.log(msg)
+              return
+            }
+            that.loadOrderList()
+          })
+        }
+      }
+    })
+  },
+  async confirmTakeGoods(e) {
+    const that = this
+    wx.showModal({
+      title: "确认收货",
+      content: '确认已经收到商品？',
+      success(res) {
+        if (res.confirm) {
+          const orderId = e.currentTarget.dataset.id
+          order.confirmTakeGoods(orderId).then(res => {
+            const { error_code, msg } = res
+            if (error_code !== undefined) {
+              console.log(msg)
+              return
+            }
+            that.loadOrderList()
+          })
+        }
+      }
+    })
+  },
+  // 切换状态
+  switchStatus(e) {
+    const status = e.currentTarget.dataset.status
+    if (parseInt(status) === this.data.status) {
+      return
+    }
+    this.setData({
+      status: parseInt(status, 10)
+    })
+    this.loadOrderList()
   }
 })
