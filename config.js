1a6b42a840672d31d2614b0788dc7d510e3d3113
dazuo <863329112@qq.com>
2020-04-04 09:31:57 +0800

    更新配置

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 export default {
   version: '0.1.0',
-  baseURL: 'http://127.0.0.1:8081',
-  shareProfile: '百款精品商品，总有一款适合您', // 首页转发的时候话术
+  baseURL: 'https://wechat-mall.maxzuo.com',
+  shopName: 'Apple商城',
+  shareProfile: 'Apple商城',
+  phoneNumber: '188700000000'
 }
